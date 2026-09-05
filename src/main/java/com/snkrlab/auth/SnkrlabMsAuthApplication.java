74b720d652eace8701c88577c52daf81eb2ffd59
carlitroxxx <ca.moil@duocuc.cl>
2026-09-04 21:39:28 -0400

    Version 1.0.0: Inicio microservicio auth

---
+++ src/main/java/com/snkrlab/auth/SnkrlabMsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.snkrlab.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnkrlabMsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnkrlabMsAuthApplication.class, args);
+	}
+
+}
